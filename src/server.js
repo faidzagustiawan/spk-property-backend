7bf0dba3619fbe4e14b67bf57c53432638da28de
faidz <faidzagustiawan@gmail.com>
2026-05-04 14:05:22 +0700

    rubah server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 const config = require('./config/env');
 
-const PORT = config.port;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server berjalan di mode development pada port ${PORT}`);
+  console.log(`Server berjalan di mode development pada port ${PORT}`);
 });
